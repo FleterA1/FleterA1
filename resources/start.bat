b0a619b8b0db21f94abd161852bc54eb2e7723b1
FleterA1 <111411732+FleterA1@users.noreply.github.com>
2022-11-26 14:14:17 +0000

    .

--- resources/start.bat
+++ resources/start.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
+net config server /srvcomment:"Fleter.." > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user Fleter Admin@ys?0504 /add >nul
@@ -12,13 +12,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant Fleter:F >nul
 ICACLS C:\Windows\installer /grant Fleter:F >nul
-echo Successfully installed! If RDP is dead, rebuild again.
+echo Successfully installed!
 echo IP:
-cd "D:\a\ngrok-rdp-one\ngrok-rdp-one\ngrok\"
-ngrok.exe authtoken 2I55cjy6KzjkPA6nrl0pyEGvEna_pQTqNjemjvXp7xWDCg2s
-powershell Start-Process Powershell -ArgumentList '-Noexit -Command ".\ngrok\ngrok.exe tcp 3389 --region ap"'
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: Fleter
-echo Password: admin
-echo You can login now
+echo Password: Admin@ys?0504
 ping -n 10 127.0.0.1 >nul
