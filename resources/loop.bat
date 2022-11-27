b2113dccd99bc33f6738b61ff7bc75573f2e67a8
FleterA1 <111411732+FleterA1@users.noreply.github.com>
2022-11-27 07:38:38 +0700

    .

--- resources/loop.bat
+++ resources/loop.bat
@@ -1,4 +1,4 @@
 :loop
-ping 127.0.0.1
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || "C:\Windows\ngrok.exe" config add-authtoken 2I55cjy6KzjkPA6nrl0pyEGvEna_pQTqNjemjvXp7xWDCg2s | "C:\Windows\ngrok.exe" tcp 3389 --region ap
 goto loop
 exit
