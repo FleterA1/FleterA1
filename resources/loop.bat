607a519aac093aec08390762e1c56c5be27a8558
FleterA1 <111411732+FleterA1@users.noreply.github.com>
2022-11-28 11:30:29 +0700

    .

--- resources/loop.bat
+++ resources/loop.bat
@@ -1,7 +1,19 @@
 @echo off
 
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || goto exit
+tasklist | find /i "ngrok.exe" && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || goto ngrok
+
 :loop
-tasklist | find /i "ngrok.exe" >Nul || goto exit
+tasklist | find /i "ngrok.exe" || goto exit
+tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels >nul | jq -r .tunnels[0].public_url || goto exit
+echo.
+TIMEOUT /T 2 >nul >nobreak
 goto loop
+
+:ngrok
+"C:\Users\Fleter\Downloads\ngrok-v3-stable-windows-amd64\ngrok.exe" config add-authtoken 2I55cjy6KzjkPA6nrl0pyEGvEna_pQTqNjemjvXp7xWDCg2s
+powershell Start-Process Powershell -ArgumentList '-Noexit -Command ""C:\Users\Fleter\Downloads\ngrok-v3-stable-windows-amd64\ngrok.exe" tcp 3389 --region ap"'
+goto loop
+
 :exit
+pause >null
+exit
