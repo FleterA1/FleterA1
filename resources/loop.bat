d8fbcf72235b91e7cded7bb95364453092e62574
FleterA1 <111411732+FleterA1@users.noreply.github.com>
2022-11-28 10:52:58 +0700

    .

--- resources/loop.bat
+++ resources/loop.bat
@@ -1,6 +1,7 @@
 @echo off
 
-:loop
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || goto exit
+:loop
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels |  || goto exit
 goto loop
 :exit
